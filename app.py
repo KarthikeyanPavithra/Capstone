e980352f36f408d64fa978c9f021b41c17e2cfe8
KarthikeyanPavithra <pavithra10697@gmail.com>
2025-02-12 02:17:03 +0530

    changed to app

--- app.py
+++ app.py
@@ -155,7 +155,6 @@ def create_app(test_config=None):
 
       return app
 
-APP = create_app()
-
 if __name__ == '__main__':
-    APP.run(debug=True)
+    app = create_app()
+    app.run(debug=True)
